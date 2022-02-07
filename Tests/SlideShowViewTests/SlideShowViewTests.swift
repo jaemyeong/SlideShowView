48da569fa263f27df4beed769e2dc69e8454f3a2
Jaemyeong <jaemyeong@me.com>
2022-02-07 14:36:10 +0900

    프로젝트 생성

---
+++ Tests/SlideShowViewTests/SlideShowViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SlideShowView
+
+final class SlideShowViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SlideShowView().text, "Hello, World!")
+    }
+}
